a577953468b1b909a69e9e1f26cb5452b5a7b6e5
Starry Zhou <starryzhou@MBA-00118.local>
2019-07-25 15:13:51 -0400

    first step of renames

--- data_migration.R
+++ data_migration.R
@@ -1,7 +1,7 @@
 library(readxl)
 library(tidyverse)
 
-# commits_export
+# commits export
 commits_2013 <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_grants_2013_proposals.xlsx") %>% 
   rename("GRANT_STATUS__C" = "Application Status") %>% 
   rename("GRANT_ID__C" = "External Proposal ID") %>% 
@@ -20,7 +20,7 @@ commits_2013 <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_gran
          `PAYMENT_STATUS__C`, `GRANT_START_DATE__C`, `PROGRAM__C`, `GRANT_END_DATE__C`, 
          `GRANT_STATUS__C`, `GRANTED_INSTITUTION__C`, `DISBURSEMENT_REQUEST_AMOUNT__C`)
 
-# proposal_export
+# proposal export
 proposal_2013 <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_grants_2013_proposals.xlsx") %>% 
   rename("NAME" = "Grant Title") %>% 
   rename("AMOUNT_REQUESTED__C" = "Amount Requested") %>% 
@@ -42,8 +42,45 @@ proposal_2013 <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_gra
          PROJECT_DESCRIPTION_PROPOSAL_ABSTRACT__C, ZENN_ID__C, STATUS__C, PROGRAM__C)
   
 # team export
-task_2013 <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_grants_2013_proposals.xlsx") %>% 
+team_2013 <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_grants_2013_proposals.xlsx") %>% 
   rename("NAME" = "Grant Title") %>% 
   rename("PROPOSAL_TOTAL_FUNDED_AWARD_AMOUNT__C" = "Amount Approved") %>% 
   mutate("END_DATE_OF_FIRST_PROGRAM_COMPLETED__C" = as.Date(`Actual Period End`)) %>% 
   select(NAME, PROPOSAL_TOTAL_FUNDED_AWARD_AMOUNT__C, END_DATE_OF_FIRST_PROGRAM_COMPLETED__C)
+
+# team membership export 
+membership_2013_1a <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_grants_2013_proposals.xlsx") %>% 
+  mutate("START_DATE__C" = as.Date(`Actual Period Begin`)) %>% 
+  mutate("END_DATE__C" = as.Date(`Actual Period End`)) %>% 
+  rename(
+    "TEAM_NAME_TEXT_ONLY_HIDDEN__C" = "Grant Title",
+    "PROPOSAL_STATUS__C" ="Application Status") %>% 
+  select(START_DATE__C, END_DATE__C, TEAM_NAME_TEXT_ONLY_HIDDEN__C, PROPOSAL_STATUS__C)
+
+membership_2013_1b <- proposal_2013 %>% 
+  select(NAME, ZENN_ID__C) %>% 
+  rename("TEAM_NAME_TEXT_ONLY_HIDDEN__C" = "NAME") 
+
+membership_2013_2 <- merge(membership_2013_1a, membership_2013_1b) # add ZENN ID
+
+advisors <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_grants_2013_advisors.xlsx") %>% 
+  rename("ZENN_ID__C" = "Zenn ID")
+
+membership_2013 <- merge(membership_2013_2, advisors) %>% 
+  mutate("START_DATE__C" = as.Date(`START_DATE__C`)) %>% 
+  mutate("END_DATE__C" = as.Date(`END_DATE__C`)) %>% 
+  unite("FULL_NAME__C", c(`First Name`, `Last Name`), sep = " ", remove = FALSE) %>% 
+  mutate("PROGRAM_TYPE_FORMULA__C" = "Sustainable Vision") %>% 
+  rename(
+    "ROLE__C" = "Team Role",
+    "EMAIL_FORMULA__C" = Email,
+    "PHONE_FORMULA__C" = `Telephone 1`,
+    "FIRST_NAME__C" = `First Name`,
+    "LAST_NAME__C" = `Last Name`,
+    "ORGANIZATION__C" = Organization
+  ) %>% 
+  select(ROLE__C, START_DATE__C, END_DATE__C, FULL_NAME__C, EMAIL_FORMULA__C, 
+         PHONE_FORMULA__C, PROGRAM_TYPE_FORMULA__C, FIRST_NAME__C, ORGANIZATION__C,
+         PROPOSAL_STATUS__C, LAST_NAME__C, TEAM_NAME_TEXT_ONLY_HIDDEN__C)
+# note: status needs capitalization 
+  
