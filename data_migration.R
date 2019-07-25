84c87e7f32acb816ee183b45ac6e388d59270bdb
Starry Zhou <starryzhou@MBA-00118.local>
2019-07-25 16:02:02 -0400

    more details

--- data_migration.R
+++ data_migration.R
@@ -36,7 +36,8 @@ proposal_2013 <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_gra
     "PROGRAM_COHORT_RECORD_TYPE__C" = "Type",
     "PROJECT_DESCRIPTION_PROPOSAL_ABSTRACT__C" = "Proposal Summary",
     "ZENN_ID__C" = "Zenn ID",
-    "STATUS__C" = "Application Status"
+    "STATUS__C" = "Application Status", 
+    "EXTERNAL_PROPOSAL_ID__C" = "External Proposal ID"
     ) %>% 
   mutate(
     "PROPOSAL_NAME_LONG_VERSION__C" = as.character(NAME),
@@ -48,10 +49,12 @@ proposal_2013 <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_gra
     ) %>% 
   select(
     NAME, AMOUNT_REQUESTED__C, PROPOSAL_NAME_LONG_VERSION__C, APPLYING_INSTITUTION_NAME__C,
-         AWARD_AMOUNT__C, DATE_CREATED__C, DATE_SUBMITTED__C, GRANT_PERIOD_END__C, 
-         GRANT_PERIOD_START__C, PROGRAM_COHORT_RECORD_TYPE__C, 
-         PROJECT_DESCRIPTION_PROPOSAL_ABSTRACT__C, ZENN_ID__C, STATUS__C, PROGRAM__C
+    AWARD_AMOUNT__C, DATE_CREATED__C, DATE_SUBMITTED__C, GRANT_PERIOD_END__C, 
+    GRANT_PERIOD_START__C, PROGRAM_COHORT_RECORD_TYPE__C, 
+    PROJECT_DESCRIPTION_PROPOSAL_ABSTRACT__C, ZENN_ID__C, STATUS__C, PROGRAM__C,
+    EXTERNAL_PROPOSAL_ID__C
     )
+## needs to change proposal summary 
   
 # team 
 team_2013 <- read_excel("~/Desktop/Sustainable_Vision/sustainable_vision_grants_2013_proposals.xlsx") %>% 
@@ -115,5 +118,5 @@ membership_2013 <- merge(membership_2013_2, advisors) %>%
     PHONE_FORMULA__C, PROGRAM_TYPE_FORMULA__C, FIRST_NAME__C, ORGANIZATION__C,
     PROPOSAL_STATUS__C, LAST_NAME__C, TEAM_NAME_TEXT_ONLY_HIDDEN__C
     )
-# note: status needs capitalization 
+## note: status needs capitalization 
   
